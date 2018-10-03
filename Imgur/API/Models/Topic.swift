05f8d229b0df690e99087152cccb49c224ca22b0
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-10-03 23:20:24 +0300

    Add the topic model

---
+++ Imgur/API/Models/Topic.swift
@@ -0,0 +1,31 @@
+//
+//  Topic.swift
+//  Imgur
+//
+//  Created by Владимир Кубанцев on 29/09/2018.
+//  Copyright © 2018 voinc. All rights reserved.
+//
+
+import Foundation
+
+struct Topic: Decodable {
+    let id: Int
+    let name: String
+    let description: String?
+    let css: String?
+    let ephemeral: Bool
+    let topPost: GalleryItem?
+    let heroImage: Image?
+    let isHero: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case css
+        case ephemeral
+        case topPost = "topPost"
+        case heroImage = "heroImage"
+        case isHero = "isHero"
+    }
+}
