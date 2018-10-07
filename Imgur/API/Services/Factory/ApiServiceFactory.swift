1d719840e80cb35524243433ed2beb3f6e67ef82
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-10-07 19:46:50 +0300

    Make working factory

--- Imgur/API/Services/Factory/ApiServiceFactory.swift
+++ Imgur/API/Services/Factory/ApiServiceFactory.swift
@@ -9,12 +9,33 @@
 import Foundation
 
 final class ApiServiceFactory {
-    let environment = Environment(
-        name: "test",
-        host: "https://api.imgur.com/3"
-    )
+    let environment: Environment
+    let dispatcher: Dispatcher
+    let decoder: JSONDecoder
+    
+    static let shared = ApiServiceFactory()
     
     init() {
-        
+        environment = Environment(
+            name: "test",
+            host: "https://api.imgur.com/3",
+            headers: ["Authorization": "Client-ID 9a6574ec5eb7228"]
+        )
+        dispatcher = Dispatcher(environment: environment)
+        decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = JSONDecoder.DateDecodingStrategy.custom({ (decoder) -> Date in
+            let container = try decoder.singleValueContainer()
+            let timestamp = try container.decode(Double.self)
+            return Date(timeIntervalSince1970: timestamp)
+        })
+    }
+    
+    func createAlbumService() -> AlbumServiceProtocol {
+        return AlbumService(
+            dispatcher: dispatcher,
+            environment: environment,
+            decoder: decoder
+        )
     }
 }
